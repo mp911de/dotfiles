d904565d28991b5d6cecf07a722bc5e0dc34f9d1
Mark Paluch <mpaluch@paluch.biz>
2026-08-26 10:01:53 +0200

    Remove unused files.

--- bin/update-license-headers.sh
+++
@@ -1,4 +0,0 @@
-YEAR=2020
-for file in `find . -name '*.java' `; do sed -E -i '' "s/\* Copyright ([0-9]{4})-([0-9]{4}) the/\* Copyright \1-${YEAR} the/g" $file; done
-for file in `find . -name '*.java' `; do sed -E -i '' "s/\* Copyright ([0-9]{4}) the/\* Copyright \1-${YEAR} the/g" $file; done
-
